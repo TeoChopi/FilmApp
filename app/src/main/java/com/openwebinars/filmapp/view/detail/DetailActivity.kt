bf9bf2e732496603e5c30fcca0f38e9bb842d3bb
teo <teofilo.gonzalez@kairosds.com>
2023-04-08 19:37:37 +0200

    Add -> Base

--- app/src/main/java/com/openwebinars/filmapp/view/detail/DetailActivity.kt
+++ app/src/main/java/com/openwebinars/filmapp/view/detail/DetailActivity.kt
@@ -1,24 +1,17 @@
 package com.openwebinars.filmapp.view.detail
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.openwebinars.filmapp.R
-import com.openwebinars.filmapp.view.detail.DetailFragment.Companion.EXTRA
-import com.openwebinars.filmapp.model.Film
+import com.openwebinars.filmapp.base.Base
 import com.openwebinars.filmapp.databinding.ActivityDetailBinding
+import com.openwebinars.filmapp.model.Film
+import com.openwebinars.filmapp.view.detail.DetailFragment.Companion.EXTRA
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : Base.BaseActivity<ActivityDetailBinding>() {
 
-    private lateinit var binding: ActivityDetailBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+    override fun init() {
         getExtras()?.let {
             setFragment(DetailFragment.newInstance(it))
         } ?: finish()
@@ -31,4 +24,6 @@ class DetailActivity : AppCompatActivity() {
             .replace(R.id.layoutFragmentHolder, fragmentToChange)
             .commit()
     }
+
+    override fun getViewBinding() = ActivityDetailBinding.inflate(layoutInflater)
 }
